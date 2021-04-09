f11f95368e57a3672d0ab03c233adf3d49bcf6d2
TOMO-YOSHI <lucky.paddy10@gmail.com>
2021-04-09 02:38:50 -0700

    Implement a mutation function

--- client/src/requests.js
+++ client/src/requests.js
@@ -1,11 +1,54 @@
 const endpointURL = 'http://localhost:9000/graphql';
 
-export async function loadJob(id){
+async function graphqlRequest(query, variables={}){
     const response = await fetch(endpointURL, {
         method: 'POST',
         headers: {'content-type': 'application/json'},
-        body: JSON.stringify({
-            query: `query JobQuery($id: ID!) {
+        body: JSON.stringify({query, variables})
+    });
+    const responseBody = await response.json();
+    if (responseBody.errors) {
+        const message = responseBody.errors.map((error) => error.message).join(`\n`);
+        // console.log(message)
+        throw new Error(message);
+    }
+    return responseBody.data;
+}
+
+export async function createJob(input){
+    const mutation = `mutation CreateJob($input: CreateJobInput) {
+        job: createJob(input: $input)
+            {
+                id
+                title
+                company {
+                    id
+                    name
+                }
+            }
+    }`;
+    const { job } = await graphqlRequest(mutation, {input})
+    return job;
+}
+
+export async function loadCompany(id){
+    const query = `query CompanyQuery($id: ID!){
+        company(id:$id) {
+            id
+            name
+            description
+            jobs {
+                id
+                title
+            }
+        }
+    }`;
+    const {company} = await graphqlRequest(query, {id})
+    return company;
+}
+
+export async function loadJob(id){
+    const query = `query JobQuery($id: ID!) {
                 job(id: $id) {
                     id
                     title
@@ -15,20 +58,13 @@ export async function loadJob(id){
                     }
                     description
                 }
-            }`,
-            variables: {id}
-        })
-    });
-    const responseBody = await response.json();
-    return responseBody.data.job;
+            }`;
+    const data = await graphqlRequest(query, {id})
+    return data.job;
 }
 
-export async function loadJobs(){
-    const response = await fetch(endpointURL, {
-        method: 'POST',
-        headers: {'content-type': 'application/json'},
-        body: JSON.stringify({
-            query: `{
+export async function loadJobs() {
+    const query = `{
                 jobs {
                     id
                     title
@@ -37,9 +73,51 @@ export async function loadJobs(){
                         name
                     }
                 }
-            }`
-        })
-    });
-    const responseBody = await response.json();
-    return responseBody.data.jobs;
+            }`;
+    const {jobs} = await graphqlRequest(query);
+    return jobs;
 }
+
+// export async function loadJob(id){
+//     const response = await fetch(endpointURL, {
+//         method: 'POST',
+//         headers: {'content-type': 'application/json'},
+//         body: JSON.stringify({
+//             query: `query JobQuery($id: ID!) {
+//                 job(id: $id) {
+//                     id
+//                     title
+//                     company {
+//                         id
+//                         name
+//                     }
+//                     description
+//                 }
+//             }`,
+//             variables: {id}
+//         })
+//     });
+//     const responseBody = await response.json();
+//     return responseBody.data.job;
+// }
+
+// export async function loadJobs(){
+//     const response = await fetch(endpointURL, {
+//         method: 'POST',
+//         headers: {'content-type': 'application/json'},
+//         body: JSON.stringify({
+//             query: `{
+//                 jobs {
+//                     id
+//                     title
+//                     company {
+//                         id
+//                         name
+//                     }
+//                 }
+//             }`
+//         })
+//     });
+//     const responseBody = await response.json();
+//     return responseBody.data.jobs;
+// }
